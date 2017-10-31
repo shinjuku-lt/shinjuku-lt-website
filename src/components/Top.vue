2d473654d7f6dbb846e13c11fe1d24498fd40e7f
sgswtky <sugisawa_takaya@cyberagent.co.jp>
2017-10-31 20:13:15 +0900

    使うパッケージとかを追加した

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,23 +1,19 @@
-<template>
-    <div class="top">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="pug">
+  div#top
+    img(src="@/assets/sunjuku_lt.png")
+    h1 {{ msg }}
+    h2 Essential Links
+    ul
+      li: a(href="#" target="_blank") Core Docs
+      li: a(href="#" target="_blank") Forum
+      li: a(href="#" target="_blank") Gitter Chat
+      li: a(href="#" target="_blank") Twitter
+    h2 Ecosystem
+    ul
+      li: a(ref="#" target="_blank") vue-router
+      li: a(ref="#" target="_blank") vuex
+      li: a(ref="#" target="_blank") vue-loader
+      li: a(ref="#" target="_blank") awesome-vue
 </template>
 
 <script>
