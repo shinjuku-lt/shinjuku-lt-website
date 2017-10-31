2cbf5d3dfafdc8d5d182c860c2cd342fce28852a
sgswtky <sugisawa_takaya@cyberagent.co.jp>
2017-10-31 20:45:35 +0900

    ヘッダーを作った

---
+++ src/components/Shinjuku.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  div#shinjuku
+    img(src="../assets/sunjuku_lt.png" width="25%")
+</template>
+
+<script>
+export default {
+  name: 'Shinjuku',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
