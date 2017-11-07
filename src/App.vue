051e310465aa9d7ef7d1de20e837bb430059bea2
HomMarkHunt <honmaso@gmail.com>
2017-11-08 02:13:44 +0900

    AboutUsとContactを作成

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
 
   position: relative;
   height: 100%;
--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,6 +1,27 @@
 <template lang="pug">
   div#aboutus
-    h1 aboutus
+    h2 Shinjuku.LTとは？
+    p 
+    | Shinjuku.LTは 
+    strong 世界で一番LTのハードルが低い勉強会 
+    | を目的に開催している勉強会です。
+    br
+    | 勉強会で一番勉強になるのは、発表者と言われることがあります。
+    br
+    | ということは、発表する機会をつくるこで勉強になる人が増えるのではないかという理念を元に開催しています。
+    h2 目的
+    ol
+      li LTに慣れる
+      li 定期的にアウトプットできる環境を作る
+      li 人の話を聞いて知見を増やす
+      li エンジニア同士で仲良くなりたい
+    h2 開催場所
+    p 
+    |
+    a(href="https://www.val.co.jp/") 株式会社Val研究所様 
+    | 並びに同テクニカルエバンジェリストの
+    a(href="https://twitter.com/maruyamahiakru") 丸山ひかるさん
+    | に会場を提供していただいております。
 </template>
 
 <script>
