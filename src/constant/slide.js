ccbffd7290944f4cad319362d0f60f04e5f831d0
sgswtky <sugisawa_takaya@cyberagent.co.jp>
2017-11-07 19:57:59 +0900

    プルダウンで年月変更できるところまで

---
+++ src/constant/slide.js
@@ -0,0 +1,38 @@
+export const SLIDES = {
+  201710: [
+    {
+      name: 'ホンマークハントのスライド',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/1z6SwGsXCxmgUN',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: 'ホンマークハントのスライド2',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/1z6SwGsXCxmgUN',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201711: [
+    {
+      name: 'ハントさんのスライド',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/1z6SwGsXCxmgUN',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: 'ハントさんのスライド',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/1z6SwGsXCxmgUN',
+        width: 340,
+        height: 290
+      }
+    }
+  ]
+}
