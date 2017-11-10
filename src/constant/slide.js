ba6a23403e15c85f13b616669c501f9b57404d4c
金岡 勇佑 <kaneoka_yusuke_gn@cyberagent.co.jp>
2017-11-10 18:27:47 +0900

    slideのjson追加した

--- src/constant/slide.js
+++ src/constant/slide.js
@@ -1,35 +1,248 @@
 export const SLIDES = {
-  201710: [
+  201606: [
     {
-      name: 'ホンマークハントのスライド',
+      name: 'groovy',
+      auther: '',
       page: {
-        url: 'http://www.slideshare.net/slideshow/embed_code/key/1z6SwGsXCxmgUN',
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/mFyxbBkT8LhfFP',
         width: 340,
         height: 290
       }
     },
     {
-      name: 'ホンマークハントのスライド2',
+      name: '',
+      auther: '',
       page: {
-        url: 'http://www.slideshare.net/slideshow/embed_code/key/1z6SwGsXCxmgUN',
+        url: 'http://to4iki.github.io/slides/20160911_shinjukult/#1',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: '',
+      auther: '',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/NsAQEL6mLqyCbF',
         width: 340,
         height: 290
       }
     }
   ],
-  201711: [
+  201607: [
     {
-      name: 'ハントさんのスライド',
+      name: 'shijukult',
+      auther: '',
       page: {
-        url: 'http://www.slideshare.net/slideshow/embed_code/key/1z6SwGsXCxmgUN',
+        url: 'https://speakerdeck.com/player/42a0e16143f146b28498a3f055ac30c2?#',
         width: 340,
         height: 290
       }
     },
     {
-      name: 'ハントさんのスライド',
+      name: 'railsapi',
+      auther: '',
       page: {
-        url: 'http://www.slideshare.net/slideshow/embed_code/key/1z6SwGsXCxmgUN',
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/1wQtd2sqhP7eJx',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201610: [
+    {
+      name: 'benchmark',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/613b901fbc8545f9af2ea231253475bb?#',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: 'lineapi',
+      auther: '',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/JuOPIKhDcQ1wgW',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: '',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/6f188031181347f19d75e1fc039da823?#',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201612: [
+    {
+      name: 'simpleeacy',
+      auther: '',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/w9BXLy55Z1CEns',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201701: [
+    {
+      name: 'catgorize',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/89420f7155ee4558800c9b0bacfa1c97?#',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201702: [
+    {
+      name: 'puroresu',
+      auther: '',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/4IKLKVLMGspkI7',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: 'dsl',
+      auther: '',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/xMu6uwKB2C8QOF',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: 'contenst',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/0c7abc11188b4f5e81d72c5ead0f32ea?#',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201703: [
+    {
+      name: '',
+      auther: '',
+      page: {
+        url: 'http://www.slideshare.net/slideshow/embed_code/key/va4Cb9R7WDyaHG',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: 'devecamp',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/01511cf43dcf471db57967b7adbcf13a?#',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201705: [
+    {
+      name: 'phantom',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/ac8390bb19ba4992b9ad7f045bcaab55?#',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: 'toil',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/f07319144ddc491da9c0b5c5dba61756?#',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201706: [
+    {
+      name: 'hurikaeru',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/611c47de8a4c448f8bf73fa7a3b7c6bd?#',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: '',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/4eb0bc07e1694f9fa376d23cb83e6386?#',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201707: [
+    {
+      name: 'isucon',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/624cb8b06bf54fca8ed8b60c6739ac45?#',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201708: [
+    {
+      name: 'kanfarensu',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/a0cf9dd090d446258e28e1651df1cf71?#',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: 'hikitugi',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/f50105df0e244202882d8945c4555aeb?#',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: '',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/32d936b896ca49ffb072471f9fea75a0?#',
+        width: 340,
+        height: 290
+      }
+    },
+    {
+      name: '',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/f50105df0e244202882d8945c4555aeb?#',
+        width: 340,
+        height: 290
+      }
+    }
+  ],
+  201709: [
+    {
+      name: 'vegita',
+      auther: '',
+      page: {
+        url: 'https://speakerdeck.com/player/5d9420d156f04fcda7444bb9825d70ec?#',
         width: 340,
         height: 290
       }
