2a883ebc269244f3223079703bbe9c4d9a44000a
HikaruMaruyama <7115497+hmaruyama@users.noreply.github.com>
2019-02-11 09:37:16 +0900

    Update auth0-variables.js

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   clientId: 'PFf6KoqjypZ9TticYdcYEDeJIr2LqEVU',
   domain: 'shinjukult.auth0.com',
-  callbackUrl: 'http://localhost:8080/callback',
+  callbackUrl: 'https://shinjukult.netlify.com/callback',
   apiUrl: 'YOUR_API_IDENTIFIER'
 }
