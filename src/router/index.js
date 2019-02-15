f0c39bc83dbf5ad66b98b56dfdb34acf99f57e04
kyusuke7 <kaneoka_yusuke_gn@cyberagent.co.jp>
2019-02-15 15:20:06 +0900

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Top from '@/components/Top'
-import AboutUs from '@/components/AboutUs'
-import Members from '@/components/Members'
-import Slides from '@/components/Slides'
-import Contact from '@/components/Contact'
-import Next from '@/components/Next'
-import Callback from '@/components/Callback'
+// import AboutUs from '@/components/AboutUs'
+// import Members from '@/components/Members'
+// import Slides from '@/components/Slides'
+// import Contact from '@/components/Contact'
+// import Next from '@/components/Next'
+// import Callback from '@/components/Callback'
 
 Vue.use(Router)
 
@@ -17,39 +17,7 @@ const router = new Router({
       path: '/',
       name: 'Top',
       component: Top,
-      redirect: '/aboutus',
-      children: [
-        {
-          path: '/aboutus',
-          name: 'AboutUs',
-          component: AboutUs
-        },
-        {
-          path: '/members',
-          name: 'Members',
-          component: Members
-        },
-        {
-          path: '/slides',
-          name: 'Slides',
-          component: Slides
-        },
-        {
-          path: '/contact',
-          name: 'Contact',
-          component: Contact
-        },
-        {
-          path: '/next',
-          name: 'Next',
-          component: Next
-        },
-        {
-          path: '/callback',
-          name: 'Callback',
-          component: Callback
-        }
-      ]
+      redirect: '/aboutus'
     }
   ]
   // routes: [
